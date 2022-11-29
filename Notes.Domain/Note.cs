7259f3a45aaba11e897f3807c6fe0e33dfbf4689
DESKTOP-VVVLAIA\tolok <tolokonnikov.albert@gmail.com>
2022-11-29 13:45:30 +0200

    initial

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid ID { get; set; }
+        public Guid UserID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Details { get; set; } = string.Empty;
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+
+    }
+}
---
+++ Notes.Persistence/EntityTypeConfiguration/NoteConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
+
+namespace Notes.Persistence.EntityTypeConfiguration
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public NoteConfiguration()
+        {
+        }
+
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(note => note.ID);
+            builder.HasIndex(note => note.ID).IsUnique();
+            builder.Property(note => note.Title).HasMaxLength(250);
+        }
+    }
+}
